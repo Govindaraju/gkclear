ed34a9255595c2047e8106aabda50ad43d491fa0
Govind <govind.kalyana@gmail.com>
2024-12-10 20:17:35 +0000

    Initial checkin

---
+++ gkclear/src/main/java/com/gimpact/gkclear/GkclearApplication.java
@@ -0,0 +1,13 @@
+package com.gimpact.gkclear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkclearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GkclearApplication.class, args);
+	}
+
+}
---
+++ gkclear/src/test/java/com/gimpact/gkclear/GkclearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gimpact.gkclear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkclearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
